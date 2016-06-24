5c2b304b48ec0223a8f831148c2a2b5bc2c84f47
Thiago Xavier <thiagoxvo@gmail.com>
2016-06-24 19:45:56 -0300

    change files to deploy on heroku

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 const app = express()
 
+const port = process.env.port || 3000
+
 app.use('/public', express.static(__dirname))
 
 app.get('/hello', (req, res) => res.send('worlds'))
 
-app.listen(3000, () => console.log('makin music on 3000'))
+app.listen(port, () => console.log('makin music on 3000'))
